480f1aaaaab3aa1f22072ba374b3b196fa15e735
Keven <dake.3601@gmail.com>
2023-05-08 21:58:55 -0600

    change ws to wss for prod

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -1,4 +1,4 @@
 const API_URL = import.meta.env.PROD ? 'https://te3004-api.onrender.com' : 'http://localhost';
-const API_WS_URL = import.meta.env.PROD ? 'ws://te3004-api.onrender.com' : 'ws://localhost';
+const API_WS_URL = import.meta.env.PROD ? 'wss://te3004-api.onrender.com' : 'ws://localhost';
 
 export { API_URL, API_WS_URL };
