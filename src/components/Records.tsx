7c071cc18a030b42f003e04ec2399fdacf85eadd
Keven <dake.3601@gmail.com>
2023-05-12 13:40:28 -0600

    Records nits

--- src/components/Records.tsx
+++ src/components/Records.tsx
@@ -39,13 +39,14 @@ const Records = () => {
   }, [lastMessage, sendMessage]);
 
   useEffect(() => {
-    fetch(`${API_URL}/api/records`).then(
-      res => res.json().then(data => data as Record[])
-    ).then(
-      data => setRecords(data)
-    ).catch(
-      err => console.log(err)
-    )
+    const fetchData = async () => {
+      const data = await fetch(`${API_URL}/api/records`);
+      const json = await data.json() as Record[];
+      console.log(json);
+      setRecords(json);
+    }
+
+    fetchData().catch(err => console.log(err));
   }, []);
 
   const connectionStatus = {
@@ -55,6 +56,17 @@ const Records = () => {
     [ReadyState.CLOSED]: 'Closed',
     [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
   }[readyState];
+
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: false,
+    timeZone: "America/Monterrey"
+  };
 
   return (
     <Stack spacing={2} direction="column" sx={{ mb: 1 }} alignItems="center">
@@ -70,7 +82,7 @@ const Records = () => {
       </Stack>
       {records.map((record: Record) => (
         <Stack spacing={5} direction="row" sx={{ mb: 1 }} alignItems="center" key={record.id}>
-          <p>{new Intl.DateTimeFormat('es-MX').format(new Date(record.timestamp))}</p>
+          <p>{new Intl.DateTimeFormat('es-MX', options).format(new Date(record.timestamp))}</p>
           <p>{record.setSpeed}</p>
           <p>{record.direction}</p>
           <p>{record.speed}</p>
