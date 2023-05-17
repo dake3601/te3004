d8158d2e54da2dd8d380fd26fc1fb8f946a1d003
Keven <dake.3601@gmail.com>
2023-05-17 15:45:28 -0600

    Radio group to button control

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,9 +1,4 @@
 import { useState, useEffect } from 'react'
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 import Stack from '@mui/material/Stack';
@@ -15,6 +10,7 @@ import useWebSocket from 'react-use-websocket';
 import { API_WS_URL } from '../utils/config';
 import { connectionStatus, connectionStatusColor } from '../utils/connectionStatus';
 import CircleIcon from '@mui/icons-material/Circle';
+import { Button } from '@mui/material';
 
 const Controls = () => {
   const [speed, setSpeed] = useState(0)
@@ -38,8 +34,7 @@ const Controls = () => {
     }
   }, [lastMessage, sendMessage]);
 
-  const handleDirectionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newDirection = (event.target as HTMLInputElement).value;
+  const handleDirectionChange = (newDirection: string) => {
     setDirection(newDirection)
     sendMessage(JSON.stringify({ speed, direction: newDirection }))
   }
@@ -71,46 +66,34 @@ const Controls = () => {
         <Typography variant='h2' style={{ fontSize: 24 }}>Controls</Typography>
       </Stack>
       {!import.meta.env.PROD && <Typography>The websocket is currently: {connectionStatus[readyState]}</Typography>}
-      <FormControl>
-        <FormLabel id="motor-direction">Motor Direction</FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="motor-direction"
-          defaultValue="Stop"
-          name="radio-buttons-group"
-          value={direction}
-          onChange={handleDirectionChange}
-        >
-          <FormControlLabel value="Backwards" control={<Radio />} label="Backwards" />
-          <FormControlLabel value="Stop" control={<Radio />} label="Stop" />
-          <FormControlLabel value="Forward" control={<Radio />} label="Forward" />
-        </RadioGroup>
-      </FormControl>
-      <FormControl>
-        <FormLabel id="motor-speed">Motor Speed</FormLabel>
-        <Box sx={{ width: 300 }}>
-          <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-            <IconButton onClick={handleSpeedClick(-5)}>
-              <Remove />
-            </IconButton>
-            <Slider
-              aria-label="Speed"
-              defaultValue={0}
-              valueLabelDisplay="auto"
-              step={5}
-              min={0}
-              max={255}
-              value={speed}
-              onChange={handleSpeedChange}
-              onChangeCommitted={handleCommitedSpeedChange}
-            />
-            <IconButton onClick={handleSpeedClick(5)}>
-              <Add />
-            </IconButton>
-            <Typography>{speed}</Typography>
-          </Stack>
-        </Box>
-      </FormControl>
+      <Stack direction="row" alignItems="center" gap={1}>
+        <Button variant="contained" onClick={() => handleDirectionChange("Off")}>Off</Button>
+        <Button variant="contained" onClick={() => handleDirectionChange("Stop")}>Stop</Button>
+        <Button variant="contained" onClick={() => handleDirectionChange("Backwards")}>Backwards</Button>
+        <Button variant="contained" onClick={() => handleDirectionChange("Forward")}>Forward</Button>
+      </Stack>
+      <Box sx={{ width: 300 }}>
+        <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+          <IconButton onClick={handleSpeedClick(-5)}>
+            <Remove />
+          </IconButton>
+          <Slider
+            aria-label="Speed"
+            defaultValue={0}
+            valueLabelDisplay="auto"
+            step={5}
+            min={0}
+            max={255}
+            value={speed}
+            onChange={handleSpeedChange}
+            onChangeCommitted={handleCommitedSpeedChange}
+          />
+          <IconButton onClick={handleSpeedClick(5)}>
+            <Add />
+          </IconButton>
+          <Typography>{speed}</Typography>
+        </Stack>
+      </Box>
     </Stack>
   )
 }
