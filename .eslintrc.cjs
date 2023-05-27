a272ed97c3990319562eded2343b3272a85c6806
Keven <dake.3601@gmail.com>
2023-05-27 02:45:48 -0600

    eslint fixes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,16 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-    '@typescript-eslint/semi': [
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': 'warn',
+		'@typescript-eslint/semi': [
 			'error'
 		],
 		'@typescript-eslint/explicit-function-return-type': 'off',
@@ -26,5 +26,13 @@ module.exports = {
 		],
 		'no-case-declarations': 'off',
 		'eol-last': 2
-  },
-}
+	},
+	overrides: [
+		{
+			files: ['**/*.cjs'],
+			env: {
+				node: true,
+			},
+		}
+	]
+};
